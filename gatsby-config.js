3157ed9b004ad2598ee9f6007d40439a72279c22
Muiz <m.muiz.q@gmail.com>
2020-04-12 14:44:33 +1000

    fixed titles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Muiz's Blog`,
+    description: `Trying my coding skills with Gatsby`,
+    author: `@MuizMir`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
